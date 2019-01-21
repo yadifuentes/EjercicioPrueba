bf22f52a085a30e90aa7a3832e0ef7184e56144d
yadifuentes <46898222+yadifuentes@users.noreply.github.com>
2019-01-21 09:40:29 -0600

    Fuentes Calderón Yadira, Grupo 02

---
+++ VelocidadPromedio.py
@@ -0,0 +1,8 @@
+#Autor: Yadi Fuentes
+#Calcula la velocidad promedio en un viaje
+
+time = int(input("Teclea el tiempo del viaje: "))
+distance = int(input("Teclea la distancia del viaje :"))
+velocity = distance/time
+
+print ("La velocidad promedio es: ", velocity)
